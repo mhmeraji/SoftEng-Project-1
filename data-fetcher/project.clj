f67ad6a3280a9a3f4dad8a9b12cbfe177a59d17b
MH.Meraji <mh.meraji12@gmail.com>
2022-05-12 20:26:26 +0430

    Update table migrations

---
+++ data-fetcher/project.clj
@@ -0,0 +1,10 @@
+(defproject data-fetcher "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [clj-http "3.12.3"]
+                 [cheshire "5.10.0"]
+                 [com.taoensso/timbre "5.1.2"]]
+  :repl-options {:init-ns data-fetcher.core})
--- telegram_messages.up.sql
+++ telegram_messages.up.sql
@@ -1,33 +1,50 @@
-CREATE TABLE "messages" (
-
-       "id"                serial                                 PRIMARY KEY,
-       "message_id"        int                           NOT NULL,
-       "type"              varchar(16)                   NOT NULL,
-       "send_date"         varchar(32)                   NOT NULL,
-       "sender_id"         varchar(64)                   NOT NULL,
-       "body"              text                          NOT NULL,
-       "sender_phone"      varchar(16)                           ,
-       "sentiment_score"   int                                   ,
-       "views_count"       int                                   ,
-       "is_comment?"       boolean                       NOT NULL,
-       "comment_of"        serial                        REFERENCES messages(id) ON DELETE CASCADE,
-
-       "persisted-at"      timestamp  without time zone
-                           DEFAULT current_timestamp
+CREATE TABLE "groups_info"
+(
+  "channel_id"          varchar(32)         NOT NULL, PRIMARY KEY,
+  "channel_type"        varchar(32)         NOT NULL,
+  "fetch_count"         int                 NOT NULL,
+  "verified?"           boolean             NOT NULL
 );
 
-INSERT INTO messages("message_id", "type", "send_date", "sender_id", "body", "sender_phone", "sentiment_score", "views_count", "is_comment?")
-VALUES ('5785', 'message', '2022-04-16T00:27:31', 'Emran_Crypto', 'علاوه بر این موضوع به زودی استارت معرفی ارز های خفن و پر سود و سبد گردانی عمومی ام داریم !!',
-'989901253812', 67, 7665, FALSE
+CREATE TABLE "users_info"
+(
+  "user_id"             varchar(32)          NOT NULL, PRIMARY KEY,
+  "phone_number"        varchar(32)          NOT NULL
 );
 
-INSERT INTO messages("message_id", "type", "send_date", "sender_id", "body", "sender_phone", "sentiment_score", "views_count", "is_comment?", "comment_of")
-VALUES ('5789', 'message', '2022-04-16T00:27:35', 'Emran_Crypto_ADMIN', 'برای دریافت سیگنال های لحظه ای بازار اوراق ما را دنبال کنید',
-'989901253812', 46, 73, TRUE, 1
+
+CREATE TABLE "messages"
+(
+  "id"                  varchar(32)                    PRIMARY KEY,
+  "pinned"              boolean              NOT NULL,
+  "silent"              boolean              NOT NULL,
+  "edit_hide"           boolean              NOT NULL,
+  "noforwards"          boolean              NOT NULL,
+  "mentioned"           boolean              NOT NULL,
+  "forwards_number"     int                  NOT NULL,
+  "channel_id"          varchar(32)                    REFERENCES groups_info(id),
+  "body"                text                 NOT NULL,
+  "views"               int                  NOT NULL
 );
 
-INSERT INTO messages("message_id", "type", "send_date", "sender_id", "body", "sender_phone", "sentiment_score", "views_count", "is_comment?")
-VALUES ('523739', 'message', '2022-04-29T12:00:35', 'OMID_TRADER', 'خرید خوبی در خفولا داشتیم. کاملا خالی از فروشنده شده و تماما امروز رنج منفی زدن. امیدوارم همراهان ما در طرف خریدار بوده باشند و در دام سفته باز نیفتاده باشن.
-آرزوی موفقیت دارم براتون.',
-'989127654777', 60, 1501, FALSE
+CREATE TABLE "message-info"
+(
+  "record_id"           varchar(32)                    REFERENCES message_isin(id),
+  "sentiment_score"     int,
+  "sentiment_type"      varchar(32),
+  "trade_direction"     varchar(8)
+
+);
+
+CREATE TABLE "isins"
+(
+  "isin"                varchar(32)                  PRIMARY KEY,
+  "fa_name"             text                 NOT NULL
+);
+
+CREATE TABLE "message_isin"
+(
+  "id"                  serial                        PRIMARY KEY,
+  "message_id"          varchar(32)         REFERENCES messages(id)
+  "isin_id"             varchar(32)         REFERENCES isins(isin)
 );
