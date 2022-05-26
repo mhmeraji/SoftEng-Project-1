657473a0a9abc65a517fbfc62fdbd710d8dd2da1
MH.Meraji <mh.meraji12@gmail.com>
2022-05-26 23:55:12 +0430

    Initialize administrator

---
+++ administrator/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to administrator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ administrator/test/administrator/core_test.clj
@@ -0,0 +1,7 @@
+(ns administrator.core-test
+  (:require [clojure.test :refer :all]
+            [administrator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
